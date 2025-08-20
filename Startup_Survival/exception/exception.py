57311343ceaebdb4bd823c951c2ee9c868d003f6
Adityajain8595 <meaditya1103@gmail.com>
2025-08-20 22:55:00 +0530

    Logging and Exception Handling

---
+++ Startup_Survival/exception/exception.py
@@ -0,0 +1,18 @@
+import sys
+from Startup_Survival.logging import logger
+class StartupSurvivalException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.filename = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occurred in file: {self.filename} at line: {self.lineno} with message: {str(self.error_message)}"
+    
+if __name__ == "__main__":
+    try:
+        logger.logging.info("Logging has started")
+    except Exception as e:
+        raise StartupSurvivalException(e,sys)
